81bd3cdf508fd23ded970a32a5ecc239e49cf899
ivansukhoparov <rbcdaaa@gmail.com>
2024-06-16 03:08:33 +0200

    create: add typeORM module and provide users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './features/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const typeOrmModule = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'sa',
+  database: 'bloggers_6',
+  autoLoadEntities: true,
+  synchronize: true,
+});
+
+const appModules = [UsersModule];
 
 @Module({
-  imports: [],
+  imports: [typeOrmModule, ...appModules],
   controllers: [AppController],
   providers: [AppService],
 })
